cc09624ec52d4db9ab048f5e11efd891ef24709a
Sherifdeen Ishola <isholasherifdeen@gmail.com>
2025-11-21 12:43:40 +0100

    fix: make 'link' prop optional in SectionHeader component for better flexibility

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,7 +1,7 @@
 import { RxCaretRight } from "react-icons/rx";
 import ActionButton from "./ActionButton";
 
-const SectionHeader = ({ title, showMoreLink = true, link }: { title: string, showMoreLink?: boolean, link: string }) => (
+const SectionHeader = ({ title, showMoreLink = true, link }: { title: string, showMoreLink?: boolean, link?: string }) => (
     <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-3 mb-6">
         <h2 className="text-base md:text-lg lg:text-[24px] text-[#181D27] font-bitter !font-semibold">{title}</h2>
         {showMoreLink && (
