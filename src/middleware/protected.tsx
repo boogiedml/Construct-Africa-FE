d99d5f9b7a608750b55badc4088a5adfb852b2e6
Favour Arua <favourvalg@gmail.com>
2025-11-22 09:39:48 +0100

    added token toast

--- src/middleware/protected.tsx
+++ src/middleware/protected.tsx
@@ -1,6 +1,7 @@
 import { Navigate } from "react-router-dom";
 import { isTokenExpired } from "../utils/tokenUtils";
 import type { ReactNode } from "react";
+import { toast } from "react-toastify";
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -10,6 +11,7 @@ export default function ProtectedRoute({ children }: ProtectedRouteProps) {
   const token = localStorage.getItem("access_token");
 
   if (!token || isTokenExpired(token)) {
+    toast.error('Token has expired or is invalid, please login again')
     localStorage.removeItem("access_token");
     localStorage.removeItem("refresh_token");
     return <Navigate to="/login" replace />;
